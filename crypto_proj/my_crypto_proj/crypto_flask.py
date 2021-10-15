a64f71a1953b99582117ee59ff6404fd909f55e8
omanduj <omanduj@gmail.com>
2021-10-15 03:41:52 -0500

    Hooks up front end with render template

--- crypto_proj/my_crypto_proj/crypto_flask.py
+++ crypto_proj/my_crypto_proj/crypto_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from crypto_logic import find_best
 
 app = Flask(__name__)
@@ -9,5 +9,9 @@ def check_coinbase_api():
     return(jsonify({'coin_info_buy': x,
                     'coin_info_sell': y
                     }))
+
+@app.route("/weblook", methods = ["GET"])
+def website_looker():
+     return render_template('index.html')
 
 app.run(debug=True)
