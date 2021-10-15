0f3fc7c6af1f115f03b08663a366769d05707b64
omanduj <omanduj@gmail.com>
2021-10-15 01:43:19 -0500

    Flask implementation check

--- crypto_proj/my_crypto_proj/crypto_flask.py
+++ crypto_proj/my_crypto_proj/crypto_flask.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
-from crypto_logic import check_coinbase
+from crypto_logic import find_best
 
 app = Flask(__name__)
 
 @app.route("/coin_info", methods = ["GET"])
 def check_coinbase_api():
-    x = check_coinbase()
-    return(jsonify({'coin_info': x}))
+    x, y = find_best(['BTC-USD', 'ETH-USD', 'DOGE-USD'], ['Bitcoin', 'Etherium', 'Dogecoin'])
+    return(jsonify({'coin_info_buy': x,
+                    'coin_info_sell': y
+                    }))
 
 app.run(debug=True)
--- crypto_proj/my_crypto_proj/crypto_logic.py
+++ crypto_proj/my_crypto_proj/crypto_logic.py
@@ -92,4 +92,4 @@ def find_best(coin_symbols, coin_names):
             best_sell.append((dict_of_coins_coinbase[coin].name, dict_of_coins_coinbase[coin].exchanger_name, dict_of_coins_coinbase[coin].sell))
 
     return(best_buy, best_sell)
-find_best(['BTC-USD', 'ETH-USD', 'DOGE-USD'], ['Bitcoin', 'Etherium', 'Dogecoin'])
+# find_best(['BTC-USD', 'ETH-USD', 'DOGE-USD'], ['Bitcoin', 'Etherium', 'Dogecoin'])
