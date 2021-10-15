dbc4cf71a9b29fbdaf4b8ecd08d2304323f23704
omanduj <omanduj@gmail.com>
2021-10-14 22:17:14 -0500

    Adds find best purchase and sell location for each coin

--- crypto_proj/my_crypto_proj/crypto_logic.py
+++ crypto_proj/my_crypto_proj/crypto_logic.py
@@ -6,8 +6,9 @@ class CoinInfo():
         Parameters: None
         Return Value: None
     """
-    def __init__(self, name):
+    def __init__(self, name): #Do I need name attribute here?
         self.symbol = None
+        self.exchanger_name = None
         self.name = name
         self.purchase = None
         self.sell = None
@@ -35,19 +36,19 @@ def coinbase():
         Return Value: dict_of_coins = A dictionary whos keys are the names of the inputted coins
                     and values are CoinInfo objects containing name, symbol, sell, and purchase price
     """
-    coin_symbols = ['BTC-USD', 'ETH-USD']
-    coin_names = ['Bitcoin', 'Etherium']
+    coin_symbols = ['BTC-USD', 'ETH-USD', 'DOGE-USD']
+    coin_names = ['Bitcoin', 'Etherium', 'Dogecoin']
     dict_of_coins = {}
 
     for coin in range(len(coin_names)):
         dict_of_coins[coin_names[coin]] = CoinInfo(coin_names[coin])
         dict_of_coins[coin_names[coin]].symbol = coin_symbols[coin]
+        dict_of_coins[coin_names[coin]].exchanger_name = 'Coinbase'
         dict_of_coins[coin_names[coin]].sell = create_url_coinbase(coin_symbols[coin], 'sell')
         dict_of_coins[coin_names[coin]].purchase = create_url_coinbase(coin_symbols[coin], 'buy')
 
-    print(dict_of_coins['Bitcoin'].sell, dict_of_coins['Bitcoin'].purchase)
     return(dict_of_coins)
-coinbase()
+
 
 def blockchain():
     """Purpose: To obtain selling and purchase price of inputted coins on blockchain
@@ -55,34 +56,47 @@ def blockchain():
         Return Value: dict_of_coins = A dictionary whos keys are the names of the inputted coins
                     and values are CoinInfo objects containing name, symbol, sell, and purchase price
     """
-    coin_symbols = ['BTC-USD', 'ETH-USD']
-    coin_names = ['Bitcoin', 'Etherium']
+    coin_symbols = ['BTC-USD', 'ETH-USD', 'DOGE-USD']
+    coin_names = ['Bitcoin', 'Etherium', 'Dogecoin']
     dict_of_coins = {}
 
     for coin in range(len(coin_symbols)):
         dict_of_coins[coin_names[coin]] = CoinInfo(coin_names[coin])
         dict_of_coins[coin_names[coin]].symbol = coin_symbols[coin]
+        dict_of_coins[coin_names[coin]].exchanger_name = 'Blockchain'
         dict_of_coins[coin_names[coin]].sell, dict_of_coins[coin_names[coin]].purchase = create_url_blockchain(coin_symbols[coin])
 
-    print(dict_of_coins['Bitcoin'].sell, dict_of_coins['Bitcoin'].purchase)
     return(dict_of_coins)
 blockchain()
 
 
 def find_best():
+    """Purpose: To identify the best location to purchase and sell a given coin
+       Parameters: None
+       Return Value: best_buy = A list of the best locations to buy each respective coin,
+                     best_sell = A list of the best locations to sell each respective coin
+
+       Note: If adding a new exchanger, make sure to edit for loop to contian new dictionary's coins
+    """
     dict_of_coins_blockchain = blockchain()
     dict_of_coins_coinbase = coinbase()
-    #make a list and find the min and max of the coin
 
-    # print(dict_of_coins_blockchain['BTC-USD'].sell, dict_of_coins_blockchain['BTC-USD'].purchase, dict_of_coins_blockchain['BTC-USD'].sell, dict_of_coins_blockchain['ETH-USD'].purchase)
-    # print(dict_of_coins_coinbase['BTC-USD'].sell, dict_of_coins_coinbase['BTC-USD'].purchase, dict_of_coins_coinbase['ETH-USD'].sell, dict_of_coins_coinbase['ETH-USD'].purchase)
-    #
-    # print("The best source to sell Bitcoin is: ", max(float(dict_of_coins_blockchain['BTC-USD'].sell), float(dict_of_coins_coinbase['BTC-USD'].sell)))
-    # print("The best source to purchase Bitcoin is: ", min(float(dict_of_coins_blockchain['BTC-USD'].purchase), float(dict_of_coins_coinbase['BTC-USD'].purchase)))
-    #
-    # print("The best source to sell Etherium is: ", max(float(dict_of_coins_blockchain['BTC-USD'].sell), float(dict_of_coins_coinbase['ETH-USD'].sell)))
-    # print("The best source to purchase Etherium is: ", min(float(dict_of_coins_blockchain['BTC-USD'].purchase), float(dict_of_coins_coinbase['ETH-USD'].purchase)))
-    print(dict_of_coins_blockchain)
-    print(dict_of_coins_coinbase)
+    best_buy = []
+    best_sell = []
 
-# find_best()
+    for i in dict_of_coins_blockchain.keys():
+        if float(dict_of_coins_blockchain[i].purchase) == min(float(dict_of_coins_blockchain[i].purchase), float(dict_of_coins_coinbase[i].purchase)):
+            best_buy.append((dict_of_coins_blockchain[i].name, dict_of_coins_blockchain[i].exchanger_name, dict_of_coins_blockchain[i].purchase))
+
+        if float(dict_of_coins_coinbase[i].purchase) == min(float(dict_of_coins_blockchain[i].purchase), float(dict_of_coins_coinbase[i].purchase)):
+            best_buy.append((dict_of_coins_coinbase[i].name, dict_of_coins_coinbase[i].exchanger_name, dict_of_coins_coinbase[i].purchase))
+
+        if float(dict_of_coins_blockchain[i].sell) == max(float(dict_of_coins_blockchain[i].sell), float(dict_of_coins_coinbase[i].sell)):
+            best_sell.append((dict_of_coins_blockchain[i].name, dict_of_coins_blockchain[i].exchanger_name, dict_of_coins_blockchain[i].sell))
+
+        if float(dict_of_coins_coinbase[i].sell) == max(float(dict_of_coins_blockchain[i].sell), float(dict_of_coins_coinbase[i].sell)):
+            best_sell.append((dict_of_coins_coinbase[i].name, dict_of_coins_coinbase[i].exchanger_name, dict_of_coins_coinbase[i].sell))
+
+    return (best_buy, best_sell)
+
+find_best()
